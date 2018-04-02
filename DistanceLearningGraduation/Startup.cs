e9533de618d104fcf7698101af8d2fde180ad658
Diana <Diana@DESKTOP-RLN4LA8>
2018-04-02 17:56:39 +0400

    Add project files.

---
+++ DistanceLearningGraduation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DistanceLearningGraduation.Startup))]
+namespace DistanceLearningGraduation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
