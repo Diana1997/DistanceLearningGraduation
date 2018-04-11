73ce5deeb614da1f948479f6b10c4e16fb1968ea
Diana <Diana@DESKTOP-3FSOTT0>
2018-04-11 08:39:51 +0400

    Test controller

---
+++ DistanceLearningGraduation/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using DistanceLearningGraduation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DistanceLearningGraduation.Controllers
+{
+    public class TestController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
