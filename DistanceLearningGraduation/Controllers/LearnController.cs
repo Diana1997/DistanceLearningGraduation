56db7179bd0ec695bbf959010c939e9f10fc238e
Diana <Diana@DESKTOP-3FSOTT0>
2018-04-11 08:32:44 +0400

    Hayereni grancum

--- DistanceLearningGraduation/Controllers/LearnController.cs
+++ DistanceLearningGraduation/Controllers/LearnController.cs
@@ -15,9 +15,20 @@ namespace DistanceLearningGraduation.Controllers
         public ActionResult Index()
         {
             var currentUserId = User.Identity.GetUserId();
-            var CourseID = db.UserCourse.Where(x => x.UserID == currentUserId).First().CourseID;
-            ViewBag.LessonList = db.Lessons.Where(x => x.CourseID == CourseID).ToList();
+            var CourseList = db.UserCourse.Where(x => x.UserID == currentUserId);
+            var CourseID = CourseList.Count() > 0 ? CourseList.First().CourseID : 0;
+            if (CourseID > 0)
+                ViewBag.LessonList = db.Lessons.Where(x => x.CourseID == CourseID).ToList();
+            else
+                ViewBag.LessonList = "";
+            ViewBag.LectureList = "";
             return View();
+        }
+
+        public void GetLecturesByLessonId(int? id)
+        {
+            ViewBag.LectureList = db.Lectures.Where(x => x.LessonID == id);
+          //  return View();
         }
     }
 }
