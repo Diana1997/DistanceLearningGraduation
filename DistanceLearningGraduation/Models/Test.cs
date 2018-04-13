bffe164cde0e5675d7708fb3f07929a0cb2727e7
Diana <Diana@DESKTOP-RLN4LA8>
2018-04-13 18:53:04 +0400

    --

---
+++ DistanceLearningGraduation/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DistanceLearningGraduation.Models
+{
+    public class Test
+    {
+        public Question Question { set; get; }
+        public IList<Answer> Answers { set; get; }
+    }
+}
